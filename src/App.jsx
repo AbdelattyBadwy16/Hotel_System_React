4b8ab88c7b38f259a8f7689ef7eb3f776da0a928
Abdelatty <abdelattybadwy166@gmail.com>
2023-09-28 14:12:49 +0300

    update react query

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,24 @@ import Account from "./pages/Account";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+    }
+  }
+});
 
 function App() {
 
   return (
-    <>
+
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false}></ReactQueryDevtools>
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -27,12 +40,27 @@ function App() {
             <Route path="settings" element={<Settings />} />
             <Route path="account" element={<Account />} />
           </Route>
-
           <Route path="login" element={<Login />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
+      <Toaster position="top-center" gutter={12} containerStyle={{margin:"8px"}} toastOptions={{
+        success : {
+          duration : 3000,
+        },
+        error : {
+          duration : 5000,
+        },
+        style :{
+          fontSize : "16px",
+          maxWidth:"500px",
+          padding : "16px 24px",
+          backgroundColor : 'var(--color-gray-0)',
+          Color : 'var(--color-gray-700)',
+        }
+      }}></Toaster>
+    </QueryClientProvider>
+
   );
 }
 
